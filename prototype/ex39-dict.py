5e6f33b24e27feaeabb509bcf3bd94f810c0be00
ateBitBug <escapedaweb@yahoo.com>
2020-12-16 17:30:43 -0500

    example

---
+++ prototype/ex39-dict.py
@@ -0,0 +1,18 @@
+# create a mapping of state to abbreviation
+states = {
+  'oregon': 'or',
+  'florida': 'fl',
+  'new york': 'ny'
+}
+
+cities = {
+  'ca': 'san francisco',
+  'mi': 'detroit',
+  'fl': 'jacksonville'
+}
+
+# add more cities
+cities['ny'] = 'new york'
+cities['or'] = 'portland'
+
+print(type(cities))
